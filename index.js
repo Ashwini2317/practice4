04024e854212dc35d6edf499554e4c8f322ffd81
ashwini <adpaithane23@gmail.com>
2025-06-22 15:39:32 +0530

    ldfsod

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express()
 
 app.use(express.json())
 app.use(express.static("dist"))
-app.use(cors({ origin: "http://localhost:5173" }))
+app.use(cors({ origin: "http://localhost:5173", credentials: true }))
 app.use("/api/user", require("./routes/user.routes"))
 
 mongoose.connect(process.env.MONGO_URL)
