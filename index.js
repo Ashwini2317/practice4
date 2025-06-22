dd6df51d15858a880a0c70774e3a27fdf3e0fd32
ashwini <adpaithane23@gmail.com>
2025-06-22 15:16:10 +0530

    update

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express")
+const cors = require("cors")
 const mongoose = require("mongoose")
 require("dotenv").config()
 const app = express()
 
 app.use(express.json())
 app.use(express.static("dist"))
+app.use(cors({ origin: "https://practice4-sof0.onrender.com" }))
 app.use("/api/user", require("./routes/user.routes"))
 
 mongoose.connect(process.env.MONGO_URL)
